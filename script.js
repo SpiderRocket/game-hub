19e1e0c8294ca414b3b7aec8bfa9009470d42ceb
SpiderRocket <caleb.1628167@nv.ccsd.net>
2024-11-18 07:25:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,98 +1,34 @@
-<!DOCTYPE html>
-<html>
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <title>Cool Site Hub</title>
-  <link href="style.css" rel="stylesheet" type="text/css" />
-</head>
+function openEmbed(evt, embed) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-<body>
-  <div class="page_content">
-    <div class="tab">
-      <button class="tablinks" onclick="openEmbed(event, 'Empty tab')">Close tabs</button>
-      <button class="tablinks" onclick="openEmbed(event, 'Starblast.io')">Starblast.io</button>
-      <button class="tablinks" onclick="openEmbed(event, 'Tribals.io')">Tribals.io</button>
-      <button class="tablinks" onclick="openEmbed(event, 'Zombs Royale')">Zombs Royale</button>
-      <button class="tablinks" onclick="openEmbed(event, 'Elgoog.im')">Elgoog.im</button>
-      <button class="tablinks" onclick="openEmbed(event, 'Yohoho.io')">Yohoho.io</button>
-      <button class="tablinks" onclick="openEmbed(event, 'Tetris.com')">Tetris</button>
-      <button class="tablinks" onclick="openEmbed(event, 'Free Rider')">Free Rider</button>
-      <button class="tablinks" onclick="openEmbed(event, 'Shell Shockers')">Shell Shockers</button>
-      <button class="tablinks" onclick="openEmbed(event, 'TGC')">TGC</button>
-      <button class="tablinks" onclick="openEmbed(event, 'Clicker Heroes')">Clicker Heroes</button>
-      <button class="tablinks" onclick="openEmbed(event, 'moomoo.io')">MooMoo.io</button>
-      <button class="tablinks" onclick="openEmbed(event, 'mario64')">Mario 64</button>
-      <button class="tablinks" onclick="openEmbed(event, 'jellymario')">Jelly Mario</button>
-      <button class="tablinks" onclick="openEmbed(event, 'updates')">Releases & Updates</button>
-      <button class="tablinks" onclick="toggleFullscreen">Fullscreen</button>
-      <button class="tablinks" onclick="openEmbed(event, 'about')">About</button>
-      <button class="tablinks" onclick="reloadPage()">Reload Page</button>
-      <form action="https://forms.gle/u3yeU83Hm6Vm1tNi7" target="_blank">
-        <button type="submit">Request a Game</button>
-      </form>
-    </div>
-  </div>
-  
-  <!-- Tab content -->
-  <div id="Empty tab" class="tabcontent">
-    <br>
-  </div>
-  <div id="Starblast.io" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://starblast.io' width=100% height=1000px>
-  </div>
-  <div id="Tribals.io" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://tribals.io' width=100% height=1000px>
-  </div>
-  <div id="Zombs Royale" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://zombsroyale.io' width=100% height=1000px>
-  </div>
-  <div id="Elgoog.im" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://elgoog.im' width=100% height=1000px>
-  </div>
-  <div id="Yohoho.io" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://yohoho.io' width=100% height=1000px>
-  </div>
-  <div id="Tetris.com" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://tetris.com' width=100% height=1000px>
-  </div>
-  <div id="Free Rider" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://freeriderhd.com' width=100% height=1000px>
-  </div>
-  <div id="Shell Shockers" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://mathdrills.info' width=100% height=1000px>
-  </div>
-  <div id="TGC" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://tgcofficial.github.io' width=100% height=1000px>
-  </div>
-  <div id="Clicker Heroes" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://clickergames.com/game/clicker-heroes' width=100% height=1000px>
-  </div>
-  <div id="moomoo.io" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://moomoo.io' width=100% height=1000px>
-  </div>
-    <div id="mario64" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://augustberchelmann.com/mario' width=100% height=1000px>
-  </div>
-      <div id="jellymario" class="tabcontent">
-    <embed class="embed1" type='text/html' src='https://jellymar.io' width=100% height=1000px>
-  </div>
-  <div id="updates" class="tabcontent">
-    <p>1.0 - Release</p><br>
-    <br>
-    <p>1.1 - Added Super Mario 64 and Jelly Mario</p><br>
-    <br>
-    <p>1.1.1 - Added About and Releases & Updates page</p>
-  </div>
-  <div id="about" class="tabcontent">
-    <p>This is a minor hosting website.  Click on the tabs to start playing a game.</p><br>
-    <br>
-    <p><strong>The Benefits:</strong></p><br>
-    <p>When you navigate to another game, your previous game does not close out.  Because of this, you can have multiple games running at once.  You can also request new games using the 'Request a Game' button.  I check this form regularly, so the game will most likely be added.  However, some games are inaccessible, and if so, I will send an email describing why the game cannot be added.</p><br>
-  </div>
-  <p>WARNING! None of this content is created by me and all rights belong to the owners or creators</p>
-  <script src="script.js"></script>
-</body>
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
-</html>
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(embed).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+}
+function reloadPage() {
+  window.location.reload();
+}
+
